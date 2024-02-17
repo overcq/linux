0b51922a426ac8443878ddaff7157bede71a83a1
Janusz Augustyński <ocq@tutanota.com>
2024-02-17 14:23:09 +0100

    ouxfs: H_oux_E_fs_Q_device_M: odczyt tablicy bloków

--- oux/fs/fs.c
+++ oux/fs/fs.c
@@ -6,90 +6,159 @@
 *         main
 * ©overcq                on ‟Gentoo Linux 17.1” “x86_64”             2024‒1‒29 L
 *******************************************************************************/
-#include <linux/fs.h>
-#include <linux/fs_context.h>
+#include <linux/blkdev.h>
 #include <linux/syscalls.h>
 #include "fs.h"
 //==============================================================================
-uint64_t H_oux_E_fs_S_file_table_start;
-//------------------------------------------------------------------------------
-struct H_oux_E_fs_Z_block *H_oux_E_fs_S_block_table;
-uint64_t H_oux_E_fs_S_block_table_n;
-//------------------------------------------------------------------------------
-struct H_oux_E_fs_Z_file *H_oux_E_fs_S_file;
-uint64_t H_oux_E_fs_S_file_n;
-struct H_oux_E_fs_Z_directory *H_oux_E_fs_S_directory;
-uint64_t H_oux_E_fs_S_directory_n;
+struct H_oux_E_fs_Q_device_Z
+{ struct bdev_handle *bdev_handle;
+  struct H_oux_E_fs_Z_block *block_table;
+  uint64_t block_table_n;
+  struct H_oux_E_fs_Z_file *file;
+  uint64_t file_n;
+  struct H_oux_E_fs_Z_directory *directory;
+  uint64_t directory_n;
+} *H_oux_E_fs_Q_device_S;
+unsigned H_oux_E_fs_Q_device_S_n;
 //==============================================================================
-static
-int
-H_oux_E_fs_Q_fs_context_I_fill_super( struct super_block *sb
-, struct fs_context *fc
-){  
-    return 0;
-}
-static
-int
-H_oux_E_fs_Q_fs_context_I_get_tree( struct fs_context *fc
-){  return get_tree_bdev( fc, H_oux_E_fs_Q_fs_context_I_fill_super );
-}
-static
-void
-H_oux_E_fs_Q_fs_context_I_free( struct fs_context *fc
-){
-}
-//------------------------------------------------------------------------------
-static const
-struct fs_context_operations
-H_oux_E_fs_S_context_ops =
-{ .get_tree = H_oux_E_fs_Q_fs_context_I_get_tree
-, .free = H_oux_E_fs_Q_fs_context_I_free
-};
-//------------------------------------------------------------------------------
-static
-int
-H_oux_E_fs_Q_type_I_init_fs_context( struct fs_context *fc
-){  fc->ops = &H_oux_E_fs_S_context_ops;
-    return 0;
-}
-static
-void
-H_oux_E_fs_Q_type_I_kill_sb( struct super_block *sb
-){  
-    kill_block_super(sb);
-}
-//------------------------------------------------------------------------------
-static
-struct file_system_type
-H_oux_E_fs_S_type =
-{ .owner = THIS_MODULE
-, .name = "ouxfs"
-, .init_fs_context = H_oux_E_fs_Q_type_I_init_fs_context
-, .kill_sb = H_oux_E_fs_Q_type_I_kill_sb
-};
-//------------------------------------------------------------------------------
 static
 int
 __init
 H_oux_E_fs_M( void
-){  return register_filesystem( &H_oux_E_fs_S_type );
+){  H_oux_E_fs_Q_device_S = kmalloc_array( 0, sizeof( *H_oux_E_fs_Q_device_S ), GFP_KERNEL );
+    return H_oux_E_fs_Q_device_S ? 0 : -ENOMEM;
 }
 static
 void
 __exit
 H_oux_E_fs_W( void
-){  unregister_filesystem( &H_oux_E_fs_S_type );
+){  kfree( H_oux_E_fs_Q_device_S );
 }
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-SYSCALL_DEFINE2( H_oux_E_fs_Q_file_I_lock, uint64_t, uid, int, operation
+int
+H_oux_E_fs_Q_device_I_read_sector( uint64_t sector
+, struct page *page
+){  struct bio *bio = bio_alloc( H_oux_E_fs_Q_device_S[ H_oux_E_fs_Q_device_S_n - 1 ].bdev_handle->bdev, 1, REQ_OP_READ, GFP_KERNEL );
+    bio->bi_iter.bi_sector = 4 * 2 * sector;
+    if( bio_add_page( bio, page, H_oux_E_fs_S_sector_size, 0 ))
+    {   bdev_release( H_oux_E_fs_Q_device_S[ H_oux_E_fs_Q_device_S_n - 1 ].bdev_handle );
+        H_oux_E_fs_Q_device_S[ H_oux_E_fs_Q_device_S_n - 1 ].bdev_handle = 0; // Zamiast realokacji.
+        return -ENOMEM;
+    }
+    struct completion event;
+    init_completion( &event );
+    bio->bi_private = &event;
+    submit_bio(bio);
+    wait_for_completion( &event );
+    bio_put(bio);
+    return 0;
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+SYSCALL_DEFINE1( H_oux_E_fs_Q_device_M, char __user *, pathname
+){  struct bdev_handle *bdev_handle = bdev_open_by_path( pathname, BLK_OPEN_READ | BLK_OPEN_WRITE | BLK_OPEN_EXCL, 0, 0 );
+    if( IS_ERR( bdev_handle ))
+        return PTR_ERR( bdev_handle );
+    unsigned device_i;
+    for( device_i = 0; device_i != H_oux_E_fs_Q_device_S_n; device_i++ )
+        if( !H_oux_E_fs_Q_device_S[ device_i ].bdev_handle )
+        {   H_oux_E_fs_Q_device_S[ device_i ].bdev_handle = bdev_handle;
+            return device_i;
+        }
+    void *p = krealloc_array( H_oux_E_fs_Q_device_S, H_oux_E_fs_Q_device_S_n, sizeof( *H_oux_E_fs_Q_device_S ), GFP_KERNEL );
+    if( !p )
+        return -ENOMEM;
+    H_oux_E_fs_Q_device_S = p;
+    H_oux_E_fs_Q_device_S_n++;
+    H_oux_E_fs_Q_device_S[ H_oux_E_fs_Q_device_S_n - 1 ].bdev_handle = bdev_handle;
+    int error;
+    struct page *page = alloc_page( GFP_KERNEL );
+    if( !page )
+    {   error = -ENOMEM;
+        goto Error;
+    }
+    error = H_oux_E_fs_Q_device_I_read_sector( 0, page );
+    if(error)
+        goto Error;
+    char *sector = page_address(page);
+    if( strncmp( sector, H_oux_E_fs_Q_device_S_ident, sizeof( H_oux_E_fs_Q_device_S_ident )))
+    {   error = -ENODEV;
+        goto Error;
+    }
+    // Odczyt tablicy bloków do pamięci operacyjnej.
+    uint64_t *block_table_n = H_oux_J_align_p( sector, uint64_t );
+    H_oux_E_fs_Q_device_S[ H_oux_E_fs_Q_device_S_n - 1 ].block_table_n = *block_table_n;
+    struct H_oux_E_fs_Z_block *block_table = H_oux_J_align_p(( char * )block_table_n + sizeof( H_oux_E_fs_Q_device_S_ident ), struct H_oux_E_fs_Z_block );
+    p = kmalloc_array( H_oux_E_fs_Q_device_S[ H_oux_E_fs_Q_device_S_n - 1 ].block_table_n, sizeof( *H_oux_E_fs_Q_device_S[ H_oux_E_fs_Q_device_S_n - 1 ].block_table ), GFP_KERNEL );
+    if( !p )
+    {   error = -ENOMEM;
+        goto Error;
+    }
+    H_oux_E_fs_Q_device_S[ H_oux_E_fs_Q_device_S_n - 1 ].block_table = p;
+    uint64_t n = H_oux_J_min( H_oux_E_fs_Q_device_S[ H_oux_E_fs_Q_device_S_n - 1 ].block_table_n, ( H_oux_E_fs_S_sector_size - ( uint64_t )block_table ) / sizeof( *block_table ));
+    for( uint64_t i = 0; i != n; i++ ) // Czyta wpisy pliku tablicy bloków znajdujące się w pierwszym sektorze.
+        H_oux_E_fs_Q_device_S[ H_oux_E_fs_Q_device_S_n - 1 ].block_table[i] = block_table[i];
+    if( H_oux_E_fs_Q_device_S[ H_oux_E_fs_Q_device_S_n - 1 ].block_table_n > n )
+    {   struct H_oux_E_fs_Z_block *block_table = ( void * )sector;
+        for( uint64_t block_table_i = n; block_table_i != H_oux_E_fs_Q_device_S[ H_oux_E_fs_Q_device_S_n - 1 ].block_table_n; ) // Czyta wszystkie pozostałe wpisy pliku tablicy bloków.
+            for( uint64_t sector_i = 0; sector_i != H_oux_E_fs_Q_device_S[ H_oux_E_fs_Q_device_S_n - 1 ].block_table[ block_table_i ].n; sector_i++ ) // Czyta kolejne sektory z szeregu ciągłych.
+            {   error = H_oux_E_fs_Q_device_I_read_sector( H_oux_E_fs_Q_device_S[ H_oux_E_fs_Q_device_S_n - 1 ].block_table[ block_table_i ].sector + sector_i, page );
+                if(error)
+                    goto Error;
+                uint64_t n = H_oux_J_min( H_oux_E_fs_Q_device_S[ H_oux_E_fs_Q_device_S_n - 1 ].block_table_n - block_table_i, H_oux_E_fs_S_sector_size / sizeof( *block_table ));
+                for( uint64_t i = 0; i != n; i++ ) // Czyta wpisy pliku tablicy bloków znajdujące się w bieżącym sektorze.
+                    H_oux_E_fs_Q_device_S[ H_oux_E_fs_Q_device_S_n - 1 ].block_table[ block_table_i++ ] = block_table[i];
+            }
+    }
+    
+    free_page(( unsigned long )sector );
+    return H_oux_E_fs_Q_device_S_n - 1;
+Error:
+    bdev_release( H_oux_E_fs_Q_device_S[ H_oux_E_fs_Q_device_S_n - 1 ].bdev_handle );
+    if( device_i != H_oux_E_fs_Q_device_S_n - 1 )
+        H_oux_E_fs_Q_device_S[ device_i ].bdev_handle = 0;
+    else
+    {   unsigned device_i;
+        for( device_i = H_oux_E_fs_Q_device_S_n - 2; device_i != ~0; device_i-- )
+            if( H_oux_E_fs_Q_device_S[ device_i ].bdev_handle )
+                break;
+        void *p = krealloc_array( H_oux_E_fs_Q_device_S, ++device_i, sizeof( *H_oux_E_fs_Q_device_S ), GFP_KERNEL );
+        if( !p )
+            return ENOMEM;
+        H_oux_E_fs_Q_device_S = p;
+        H_oux_E_fs_Q_device_S_n = device_i;
+    }
+    return error;
+}
+SYSCALL_DEFINE1( H_oux_E_fs_Q_device_W, unsigned, device_i
+){  if( device_i >= H_oux_E_fs_Q_device_S_n )
+        return -EINVAL;
+    
+    bdev_release( H_oux_E_fs_Q_device_S[ device_i ].bdev_handle );
+    if( device_i != H_oux_E_fs_Q_device_S_n - 1 )
+        H_oux_E_fs_Q_device_S[ device_i ].bdev_handle = 0;
+    else
+    {   unsigned device_i;
+        for( device_i = H_oux_E_fs_Q_device_S_n - 2; device_i != ~0; device_i-- )
+            if( H_oux_E_fs_Q_device_S[ device_i ].bdev_handle )
+                break;
+        void *p = krealloc_array( H_oux_E_fs_Q_device_S, ++device_i, sizeof( *H_oux_E_fs_Q_device_S ), GFP_KERNEL );
+        if( !p )
+            return ENOMEM;
+        H_oux_E_fs_Q_device_S = p;
+        H_oux_E_fs_Q_device_S_n = device_i;
+    }
+    return 0;
+}
+//------------------------------------------------------------------------------
+SYSCALL_DEFINE3( H_oux_E_fs_Q_file_I_lock, unsigned, dev, uint64_t, uid, int, operation
 ){  
     return 0;
 }
-SYSCALL_DEFINE4( H_oux_E_fs_Q_file_I_read, uint64_t, uid, uint64_t, sector, uint64_t, count, char __user *, data
+SYSCALL_DEFINE5( H_oux_E_fs_Q_file_I_read, unsigned, dev, uint64_t, uid, uint64_t, sector, uint64_t, count, char __user *, data
 ){  
     return 0;
 }
-SYSCALL_DEFINE4( H_oux_E_fs_Q_file_I_write, uint64_t, uid, uint64_t, sector, uint64_t, count, const char __user *, data
+SYSCALL_DEFINE5( H_oux_E_fs_Q_file_I_write, unsigned, dev, uint64_t, uid, uint64_t, sector, uint64_t, count, const char __user *, data
 ){  
     return 0;
 }
--- oux/fs/fs.h
+++ oux/fs/fs.h
@@ -6,9 +6,16 @@
 *         header
 * ©overcq                on ‟Gentoo Linux 17.1” “x86_64”             2024‒1‒29 L
 *******************************************************************************/
+#define H_oux_E_fs_Q_device_S_ident "OUXFS"
+#define H_oux_E_fs_S_sector_size    4096
+//==============================================================================
+#define H_oux_J_min(a,b)            ( (a) > (b) ? (b) : (a) )
+#define H_oux_J_max(a,b)            ( (a) < (b) ? (b) : (a) )
+#define H_oux_J_align_p(p,t)        (( void * )((( uint64_t )(p) + sizeof(t) - 1 ) % sizeof(t) ))
+//==============================================================================
 struct H_oux_E_fs_Z_block
 { uint64_t sector;
-  uint64_t size;
+  uint64_t n;
 };
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 struct H_oux_E_fs_Z_file
@@ -16,7 +23,7 @@ struct H_oux_E_fs_Z_file
   uint64_t parent;
   struct
   { uint64_t start;
-    uint64_t count;
+    uint64_t n;
   }block_table;
   uint64_t size;
   char *name;
